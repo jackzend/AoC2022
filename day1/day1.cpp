ae74448b3a5c3693a16f8f89f68c25b2c6f67baf
Jack Zender <jack@sliprobotics.com>
2022-12-02 11:40:51 -0500

    initial commit

---
+++ day1/day1.cpp
@@ -0,0 +1,55 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <vector>
+
+namespace day1
+{
+
+constexpr std::string_view input_fp = "/home/jack-slip/AoC2022/input/day1.txt";
+
+std::string_view getLine(const char*& _str, const char* end)
+{
+  const char* str = _str;
+  while (*str == '\n' || *str == '\r')
+  {
+    if (++str == end)
+      return (_str = str), std::string_view{};
+  }
+
+  const char* start = str;
+  while (str != end && *str != '\n' && *str != '\r')
+    ++str;
+
+  _str = str;
+  return std::string_view{start, str - start};
+}
+
+void parseInput(const char* file)
+{
+  std::vector<char> buffer;
+  std::ifstream in(file);
+  in.seekg(0, std::ios::end);
+  buffer.resize(in.tellg());
+  in.seekg(0, std::ios::beg);
+  in.read(&buffer[0], buffer.size());
+
+  const char* line = &buffer[0];
+  const char* end = line + buffer.size();
+
+  std::string_view word;
+
+  int sum = 0;
+  int max_sum = 0;
+  while (!(word = getLine(line, end)).empty())
+  {
+    std::cout << word << std::endl;
+  }
+}
+
+} // namespace day1
+
+using namespace day1;
+int main() { parseInput(input_fp.begin()); }
