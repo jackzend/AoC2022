967ea0281d4abef6d94c998f795d7cd37b65aec5
Jack Zender <jack@sliprobotics.com>
2022-12-04 14:13:39 -0500

    added a template and did day4

---
+++ day4/day4.cpp
@@ -0,0 +1,61 @@
+#include <iomanip>
+#include <utils/basic_timer.h>
+#include <utils/constexpr_map.h>
+#include <utils/parse.h>
+
+#include <ranges>
+namespace AoC2022
+{
+constexpr std::string_view input_fp = "/home/jack-slip/AoC2022/input/day4.txt";
+
+void day4(const char* fp)
+{
+  std::vector<char> buffer;
+  utils::readParse(buffer, fp);
+  const char* line = &buffer[0];
+  const char* end = line + buffer.size();
+
+  std::string_view line_sv;
+  constexpr std::string_view delim1{",-"};
+
+  int r1_beg, r1_end, r2_beg, r2_end;
+
+  int sum_p1 = 0;
+  int sum_p2 = 0;
+
+  while (!(line_sv = utils::getLine(line, end)).empty())
+  {
+    auto both_ranges = utils::splitSVPtr(line_sv, delim1);
+
+    r1_beg = utils::stringViewToInt(both_ranges[0]);
+    r1_end = utils::stringViewToInt(both_ranges[1]);
+
+    r2_beg = utils::stringViewToInt(both_ranges[2]);
+    r2_end = utils::stringViewToInt(both_ranges[3]);
+
+    // part1
+    bool check1 = r1_beg <= r2_beg and r1_end >= r2_end;
+    bool check2 = r2_beg <= r1_beg and r2_end >= r1_end;
+    if (check1 or check2)
+      ++sum_p1;
+
+    // part 2
+    bool check3 = r2_beg >= r1_beg and r2_beg <= r1_end;
+    bool check4 = r2_end >= r1_beg and r2_end <= r1_end;
+    if (check1 or check2 or check3 or check4)
+      ++sum_p2;
+  }
+  std::cout << "P1: " << sum_p1 << "\n";
+  std::cout << "P2: " << sum_p2 << "\n";
+}
+} // namespace AoC2022
+
+using namespace AoC2022;
+int main()
+{
+  std::cout << "---Day 4---\n";
+  utils::MicroSecondTimer ms_timer;
+  day4(input_fp.begin());
+  float elapsed = ms_timer.getElapsed();
+  std::cout << "Elapsed: " << elapsed << "us\n\n";
+}
--- utils/include/utils/parse.h
+++ utils/include/utils/parse.h
@@ -1,5 +1,6 @@
 #ifndef PARSE_H
 #define PARSE_H
+#include <charconv>
 #include <fstream>
 #include <string_view>
 #include <vector>
@@ -16,7 +17,7 @@ inline std::string_view getLine(const char*& _str, const char* end)
     return (_str = str), std::string_view{"\n"};
   }
 
-  if (str + 1 == end)
+  if (str + 1 == end or str == end or *str == '\0')
     return (_str = str), std::string_view{};
 
   const char* start = str;
@@ -34,6 +35,30 @@ inline void readParse(std::vector<char>& buf, const char* file)
   buf.resize(in.tellg());
   in.seekg(0, std::ios::beg);
   in.read(&buf[0], buf.size());
+}
+
+inline std::vector<std::string_view> splitSVPtr(std::string_view str, std::string_view delims)
+{
+  std::vector<std::string_view> output;
+
+  for (auto first = str.data(), second = str.data(), last = first + str.size();
+       second != last && first != last; first = second + 1)
+  {
+    second = std::find_first_of(first, last, std::cbegin(delims), std::cend(delims));
+
+    if (first != second)
+      output.emplace_back(first, second - first);
+  }
+
+  return output;
+}
+
+// DOES NOT DO ERROR CHECKING
+inline int stringViewToInt(const std::string_view sv)
+{
+  int i;
+  auto result = std::from_chars(sv.data(), sv.data() + sv.size(), i);
+  return i;
 }
 
 } // namespace utils
