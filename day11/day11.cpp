7c0ac9f1add7b356c9e9ac93856df5737e20e682
Jack Zender <jack@sliprobotics.com>
2022-12-11 10:00:14 -0500

    templte 4 day 11

---
+++ day11/day11.cpp
@@ -0,0 +1,30 @@
+#include <utils/basic_timer.h>
+#include <utils/constexpr_map.h>
+#include <utils/parse.h>
+namespace AoC2022
+{
+constexpr std::string_view input_fp = "/home/jack-slip/AoC2022/input/day11.txt";
+
+void day11(const char* fp)
+{
+  std::vector<char> buffer;
+  utils::readParse(buffer, fp);
+  const char* line = &buffer[0];
+  const char* end = line + buffer.size();
+
+  std::string_view line_sv;
+  while (!(line_sv = utils::getLine(line, end)).empty())
+  {
+  }
+}
+} // namespace AoC2022
+
+using namespace AoC2022;
+int main()
+{
+  std::cout << "---Day 11---\n";
+  utils::MicroSecondTimer ms_timer;
+  day11(input_fp.begin());
+  float elapsed = ms_timer.getElapsed();
+  std::cout << "Elapsed: " << elapsed << "us\n\n";
+}
