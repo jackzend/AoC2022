a51b8bc08bd0be04e31dbe6a834cd0b14a3c33b4
Jack Zender <jack@sliprobotics.com>
2022-12-12 12:54:52 -0500

    good start

--- day12/day12.cpp
+++ day12/day12.cpp
@@ -5,7 +5,7 @@
 
 namespace AoC2022
 {
-constexpr std::string_view input_fp = "/home/jack-slip/AoC2022/input/day12.txt";
+constexpr std::string_view input_fp = "/home/jack-slip/AoC2022/input/day12_sample.txt";
 
 void day12(const char* fp)
 {
@@ -16,29 +16,91 @@ void day12(const char* fp)
 
   std::string_view line_sv;
   std::vector<char> grid;
+  int rows = 0;
+  int cols = 0;
+
+  utils::Point2D start;
+  utils::Point2D end_pt;
+
   while (!(line_sv = utils::getLine(line, end)).empty())
   {
+    cols = line_sv.size();
+    int i = 0;
     for (auto c : line_sv)
     {
       if (c == 'S')
       {
         // process start
+        start = {i, rows};
       }
       if (c == 'E')
       {
         // process end
+        end_pt = {i, rows};
       }
       grid.push_back(c);
+      ++i;
     }
+    ++rows;
   }
 
-  auto gen_neighbors = [](const auto& grid, const auto& pos)
+  // x is col, y is row
+  auto get_idx = [cols](const utils::Point2D& pos) { return (size_t)(pos.x + pos.y * cols); };
+
+  std::function<std::vector<utils::Point2D>(const std::vector<char>&, const utils::Point2D&)>
+      gen_neighbors =
+          [get_idx, rows, cols](const std::vector<char>& grid, const utils::Point2D& pos)
   {
     std::vector<utils::Point2D> neighbors;
+    char curr = grid[get_idx(pos)];
+
+    for (int i = 0; i < 4; ++i)
+    {
+      utils::Point2D neighbor = pos;
+      switch (i)
+      {
+      case 0:
+        neighbor.y -= 1;
+        break;
+      case 1:
+        neighbor.y += 1;
+        break;
+      case 2:
+        neighbor.x -= 1;
+        break;
+      case 3:
+        neighbor.x += 1;
+        break;
+      }
+
+      if (neighbor.x < 0 || neighbor.y < 0 || neighbor.x >= cols || neighbor.y >= rows)
+      {
+        continue;
+      }
+
+      char neighbor_val = grid[get_idx(neighbor)];
+      if (curr == 'S')
+      {
+        curr = 'a';
+      }
+      else if (curr == 'E')
+      {
+        curr = 'z';
+      }
+      int val = neighbor_val - curr;
+      if (val > 1)
+      {
+        continue;
+      }
+
+      neighbors.push_back(neighbor);
+    }
+
     return neighbors;
   };
 
-  int num_steps = utils::dijkstra(grid, start, end, gen_neighbors);
+  int num_steps = utils::dijkstra<std::vector<char>, utils::Point2D, utils::Point2DHash>(
+      grid, start, end_pt, gen_neighbors);
 
   std::cout << "P1: " << num_steps << '\n';
 }
